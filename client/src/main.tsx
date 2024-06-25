e25036bc90a967520d9a41dc1ba412f33e3bebb6
andrewkohn <contact@kohnandrew.com>
2024-06-25 13:46:03 -0500

    Update: react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
+import ScrollToTop from "./hooks/ScrollToTop.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+        <ScrollToTop />
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>,
 );
